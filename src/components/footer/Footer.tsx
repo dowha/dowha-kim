a353650b0d8a943496cfdb05f18f462ae9f24d8c
Dowha Kim <dowha.kim@outlook.com>
2019-07-09 21:57:03 +0700

    Update contents

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { Container } from 'components/container/Container';
+import { Container } from "components/container/Container";
 
-import s from './Footer.scss';
+import s from "./Footer.scss";
 
 interface ISocial {
   icon: React.ReactNode;
@@ -18,25 +18,10 @@ export const Footer = ({ logo, social }: IFooterProps) => (
   <div className={s.footer}>
     <Container>
       <div className={s.footer__content}>
-        <a
-          href="https://ueno.co"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {logo}
-        </a>
-
         <ul className={s.footer__list}>
-          {social.map((item) => (
-            <li
-              key={item.to}
-              className={s.footer__item}
-            >
-              <a
-                href={item.to}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+          {social.map(item => (
+            <li key={item.to} className={s.footer__item}>
+              <a href={item.to} target="_blank" rel="noopener noreferrer">
                 {item.icon}
               </a>
             </li>
